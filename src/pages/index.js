df2e775450b6729b6e806eea44ac25faaf9427e5
fzoozai <fabian.soosaithasan@rwth-aachen.de>
2018-09-13 11:55:40 +0200

    Add Svg Component for landingpage

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,9 +2,11 @@ import React from 'react'
 import Link from 'gatsby-link'
 
 import Layout from '../components/layout'
+import HomeSvgAnimation from '../components/HomeSvgAnimation'
 
 const IndexPage = () => (
   <Layout>
+    <HomeSvgAnimation />
     <h1>Happy bithday!</h1>
     <p>Welcome to my birthday party.</p>
     <p>But first go build something great.</p>
