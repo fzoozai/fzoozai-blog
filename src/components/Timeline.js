17d542666f19b8cff5781e31b394f5809d5c6908
fzoozai <fabian.soosaithasan@rwth-aachen.de>
2018-10-07 16:17:47 +0200

    Set correct order of events from timeline

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -40,53 +40,6 @@ const Timeline = (props) => {
                             </VerticalTimelineElement>
                         )
                     })}
-      
-              {/* <VerticalTimelineElement
-                  className="vertical-timeline-element--work"
-                  date={"2011 - present"}
-                  iconStyle={{ background: '#E3F4F7', color: '#fff' }}
-                  icon={<img src={Skyline}></img>}
-                  style={{justifyContent: 'space-between'}}
-              >
-              <div>
-                  <h3 style={{fontFamily: 'Advent Pro'}} className="vertical-timeline-element-title">Departure</h3>
-                  <h4 style={{fontFamily: 'Advent Pro'}} className="vertical-timeline-element-subtitle">Stuttgart - Atlanta - Chicago</h4>
-                  <p>
-                  Creative Direction, User Experience, Visual Design, Project Management, Team Leading
-                  </p>
-                  <button id="readmore" style={{marginTop: '10pt'}} className="btn btn-primary">Read more</button>
-              </div>  
-              </VerticalTimelineElement>
-              <VerticalTimelineElement
-                  className="vertical-timeline-element--work"
-                  date="2010 - 2011"
-                  iconStyle={{ background: '#E3F4F7', color: '#fff' }}
-                  icon={<img src={Skyline}></img>}
-              >
-              <div>
-                  <h3 style={{fontFamily: 'Advent Pro'}} className="vertical-timeline-element-title">Test</h3>
-                  <h4 style={{fontFamily: 'Advent Pro'}} className="vertical-timeline-element-subtitle">San Francisco, CA</h4>
-                  <p>
-                  Creative Direction, User Experience, Visual Design, SEO, Online Marketing
-                  </p>
-                  <button id="readmore" style={{marginTop: '10pt'}} className="btn btn-primary">Read more</button>
-              </div>
-              </VerticalTimelineElement>
-              <VerticalTimelineElement
-                  className="vertical-timeline-element--work"
-                  date="2010 - 2011"
-                  iconStyle={{ background: '#E3F4F7', color: '#fff' }}
-                  icon={<img src={Skyline}></img>}
-              >
-              <div>
-                  <h3 style={{fontFamily: 'Advent Pro'}} className="vertical-timeline-element-title">Art Director</h3>
-                  <h4 style={{fontFamily: 'Advent Pro'}} className="vertical-timeline-element-subtitle">San Francisco, CA</h4>
-                  <p>
-                  Creative Direction, User Experience, Visual Design, SEO, Online Marketing
-                  </p>
-                  <Link to="aboutme"><button id="readmore" style={{marginTop: '10pt'}} className="btn btn-primary">Read more</button></Link>
-              </div>
-              </VerticalTimelineElement> */}
               <VerticalTimelineElement
                   iconStyle={{ background: '#E3F4F7', color: '#fff' }}
                   icon={<img src={Skyline}></img>}
