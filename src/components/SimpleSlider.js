1deffef9594a055df0dfa08c29b563bd4e381465
fzoozai <fabian.soosaithasan@rwth-aachen.de>
2019-04-04 08:31:49 -0500

    Fix Image-Slider component

--- src/components/SimpleSlider.js
+++ src/components/SimpleSlider.js
@@ -6,7 +6,6 @@ export default class SimpleSlider extends Component {
   constructor(props) {
     super(props);
     console.log("props", props);
-
     // console.log("lets see",props.slide[1].file.url);
   }
 
